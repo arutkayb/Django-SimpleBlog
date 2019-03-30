b7bc99e124455ac3d035abcb29203ac45f6a4876
Azmi Rutkay BIYIK <rutkay.biyik@gmail.com>
2019-03-31 01:30:14 +0300

    Post view hello function is created to return request's time

--- SimpleBlog-root/Posts/views.py
+++ SimpleBlog-root/Posts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
 
-# Create your views here.
+def hello(request):
+	now = datetime.datetime.now()
+	return HttpResponse(str(now))
--- SimpleBlog-root/SimpleBlog/urls.py
+++ SimpleBlog-root/SimpleBlog/urls.py
@@ -1,9 +1,10 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
+	path('posts/',include("Posts.urls")),
 	path('hello/',TemplateView.as_view(template_name='hello_world.html')),
     path('admin/', admin.site.urls),
 ]
